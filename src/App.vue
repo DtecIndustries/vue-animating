a16016e6dceb7e36f3c26b77bccf9f0202a12a4e
Joachim Defour <joachim.defour@icloud.com>
2020-04-22 16:17:56 +0200

    simple modal

--- src/App.vue
+++ src/App.vue
@@ -29,4 +29,32 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+input {
+  display: inline-block;
+  box-sizing: border-box;
+  width: 30%;
+  height: 1.8em;
+  padding: 0.5em;
+  margin: 0.2em;
+  font: 0.8em 'Avenir', Helvetica, sans-serif;
+}
+
+button {
+  margin: 0.5em;
+  padding: 0.8em 1.6em;
+  border: none;
+  border-radius: 0.3em;
+  background-color: #42b983;
+  color: white;
+}
+
+.modal{
+  min-width: 350px;
+  min-height: 350px;
+  background-color: #ddd;
+  padding: 10px;
+  margin: 0 auto;
+  margin-top: 20px;
+}
 </style>
